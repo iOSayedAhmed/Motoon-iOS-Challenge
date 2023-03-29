41a4a0d51e9dd40df11fc0c189e922323024ef43
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-29 22:53:08 +0200

    Create fetchPhotos() Function
    To get photos from API

--- Starter Project/Starter Project/ViewModels/PhotosListViewModel/PhotosListsViewModel.swift
+++ Starter Project/Starter Project/ViewModels/PhotosListViewModel/PhotosListsViewModel.swift
@@ -10,10 +10,35 @@ import Foundation
 class PhotosListsViewModel {
     
     
+    // MARK: - Properties
+    
+    var isLoading:Observable<Bool> = Observable(false)
+    
+    
     // MARK: - Public methods
     
     func numberOfItemsInSection() -> Int {
         return 0
     }
     
+    
+    
+    
+    // Fetch Photos from API
+    func fetchPhotos() {
+        guard let url = URL(string: NetworkConstants.shared.baseUrl + NetworkConstants.shared.key)else{return}
+        if isLoading.value ?? true {
+            return
+        }
+        print("fetchPhotos")
+        URLSession.shared.request(url:url , expecting: [PhotosModel].self) { [weak self] results in
+            self?.isLoading.value = false
+            switch results {
+            case .success(let data):
+                print(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
