7157fef1a6a61c0f62481df8d574da002ea8325d
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-29 11:55:17 +0200

    Create PhotosListsViewModel
    and create  numberOfItemsInSection Functions to return number of items In section for collecionView

---
+++ Starter Project/Starter Project/ViewModels/PhotosListViewModel/PhotosListsViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  PhotosListsViewModel.swift
+//  Starter Project
+//
+//  Created by iOSayed on 29/03/2023.
+//
+
+import Foundation
+
+class PhotosListsViewModel {
+    
+    
+    // MARK: - Public methods
+    
+    func numberOfItemsInSection() -> Int {
+        return 0
+    }
+    
+}
