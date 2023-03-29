cf7faff42eee6bd59debec37141cf598e2090390
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-29 11:53:29 +0200

    Create photoCollectionViewCell
    add Image View Reference as UIImageView (IBOutlet)

---
+++ Starter Project/Starter Project/Views/CollectionViewCells/photoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  photoCollectionViewCell.swift
+//  Starter Project
+//
+//  Created by iOSayed on 29/03/2023.
+//
+
+import UIKit
+
+class photoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
