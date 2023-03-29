097abcf79cc660d24931a5cc40897776503bde7f
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-30 00:12:14 +0200

    update Collection View Delegate Flow Layout

--- Starter Project/Starter Project/Controllers/PhotosList/PhotosListVC + Extensions.swift
+++ Starter Project/Starter Project/Controllers/PhotosList/PhotosListVC + Extensions.swift
@@ -12,9 +12,13 @@ extension PhotosListVC : UICollectionViewDelegate , UICollectionViewDataSource ,
     
     
     
+    
+    
     func setupCollectionView(){
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        
     }
     
     //reload CollectionView
@@ -33,21 +37,45 @@ extension PhotosListVC : UICollectionViewDelegate , UICollectionViewDataSource ,
     
     
      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
-    }
+          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIndintifier, for: indexPath) as! photoCollectionViewCell
+         if indexPath.item < cellDataSource.count {
+             cell.setupCell(viewModel: cellDataSource[indexPath.item])
+         }
+        
+       
+         return cell
+     }
     
-    // MARK: - Collection View Delegate Flow Layout
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemSize = (collectionView.frame.width - (collectionView.contentInset.left + collectionView.contentInset.right)) / 4
-        return CGSize(width: itemSize, height: itemSize)
-    }
     
     
     // MARK: - Collection View Delegate
     
      func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+    }
+    
+    // MARK: - Collection View Delegate Flow Layout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding:CGFloat = 5
+        let size = collectionView.bounds.width - padding
+        
+        return CGSize(width: size/3, height:size/3)
+    }
+    // Set edge insets for each section in the collection view
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    // Set minimum line spacing between cells in each row
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    // Set minimum inter-item spacing between cells in each column
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
     
